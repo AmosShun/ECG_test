3d17df82ce37bea3f2564bdc64277fe2433fa8ef
AmosShun <amos_shun@126.com>
2014-03-13 14:21:16 +0800

    阈值判断
    增加了阈值判断，能够得到RR间期和QRS间期。但没有进行阈值初始化，以及自适应阈值调整。

--- Derivative.java
+++ Derivative.java
@@ -1,15 +1,20 @@
 package ECG_test;
 
 public class Derivative {
+	private int sample_rate = 200;	//采样率
 	private int d1,d2;//一阶、二阶差分值
 	private int sum;//一阶二阶差分值相加
-	//获得一阶微分器、平滑器实例
-	Derivator derivator1 = new Derivator();
-	Smoother smoother1 = new Smoother();
-	//获得二阶微分器、平滑器实例
-	Derivator derivator2 = new Derivator();
-	Smoother smoother2 = new Smoother();
-	
+	private Derivator derivator1 = new Derivator();	//获得一阶微分器、平滑器实例
+	private Smoother smoother1 = new Smoother();
+	private Derivator derivator2 = new Derivator();	//获得二阶微分器、平滑器实例
+	private Smoother smoother2 = new Smoother();
+	private int THRESHOLD_UP = 400;	//迟滞比较器的一组阈值
+	private int THRESHOLD_DOWN = 100;
+	private int RR_counter;	//RR间期计数器
+	private int RR_interval;	//RR间期
+	private int QRS_counter;	//QRS间期计数器
+	private int QRS_duration;	//QRS间期
+	private int QRS_state;	//QRS状态机
 	
 	/*
 	 * 微分器
@@ -34,11 +39,18 @@ public class Derivative {
 		private int smooth(int data){
 			int y0;
 			x = data;
-			y0 = (x + 2*x1 + x2)/4;
+			y0 = (x + 2*x1 + x2);
 			x2 = x1;
 			x1 = x;
 			return y0;
 		}
+	}
+	
+	/*
+	 * 显示信息
+	 */
+	private void display(){
+		System.out.print("RR:"+RR_interval+"  QRS:"+QRS_duration+"\n");
 	}
 	
 	/*
@@ -49,19 +61,46 @@ public class Derivative {
 		int[] out = new int[ecg.length];
 		//逐点操作
 		for(int i=0; i<ecg.length; i++){
+			//一阶差分，整流，平滑
 			d1 = derivator1.get_derivative(ecg[i]);
 			if(d1<0)
 				d1 = -d1;
 			d1 = smoother1.smooth(d1);
+			//二阶差分，整流，平滑
 			d2 = derivator2.get_derivative(d1);
 			if(d2<0)
 				d2 = -d2;
 			d2 = smoother2.smooth(d2);
+			//将一阶、二阶差分相加
 			sum = (d1 + d2)*2;
+			//阈值判断
+			RR_counter++;
+			if(QRS_state == 0){
+				//上升沿阈值
+				if(sum>THRESHOLD_UP){
+					QRS_state = 1;
+					//计算RR间期
+					RR_interval = RR_counter*(1000/sample_rate);
+					RR_counter = 0;
+				}
+			}
+			else if(QRS_state == 1){
+				QRS_counter++;
+				//下降沿阈值
+				if(sum<THRESHOLD_DOWN){
+					QRS_state = 0;
+					//计算QRS间期
+					QRS_duration = QRS_counter*(1000/sample_rate);
+					QRS_counter = 0;
+					display();
+				}
+			}
+			
 			out[i] = sum;
 		}
 		return out;
 	}
+	
 	
 
 }
